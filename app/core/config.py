49e1273b3bcd74ef0b326cd166bbe84989eb951a
haku7a <v.a.neyaskin@gmail.com>
2026-01-09 05:17:18 +0300

    feat(config): add pydantic settings and integrate into main

---
+++ app/core/config.py
@@ -0,0 +1,8 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str
+    API_V1_STR: str = "/api/v1"
+
+    model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True)
